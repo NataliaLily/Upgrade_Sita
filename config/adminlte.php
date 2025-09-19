456f1132422bc35505fdcede186a261c5de22c79
NataliaLily <natalia.evelin.s@ukrim.ac.id>
2025-09-20 01:37:11 +0700

    logo ukrim

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,11 +64,11 @@ return [
     */
 
     'logo' => 'SI Tugas Akhir',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
-    'logo_img_xl' => null,
-    'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Admin Logo',
+    'logo_img' => 'vendor/adminlte/dist/img/logo-ukrim.png',
+    // 'logo_img_class' => 'brand-image img-circle elevation-3',
+    // 'logo_img_xl' => null,
+    // 'logo_img_xl_class' => 'brand-image-xs',
+    'logo_img_alt' => 'Logo UKRIM',
 
     /*
     |--------------------------------------------------------------------------
